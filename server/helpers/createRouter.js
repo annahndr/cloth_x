39fe29e6039168fb505d18933e7daff1a8a117a4
Anna Henderson <annahenderson@gmail.com>
2021-04-17 12:11:01 +0100

    able to post new user and encrypt password

--- server/helpers/createRouter.js
+++ server/helpers/createRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ObjectId = require("mongodb").ObjectID;
+const bcrypt = require("bcrypt");
 
 const createRouter = function(collection) {
   const router = express.Router();
@@ -15,6 +16,25 @@ const createRouter = function(collection) {
         res.json({ status: 500, error: errorResponse });
       });
   });
+
+  router.post("/register", (req, res)=> {
+    const newUser = req.body;
+    const saltRounds = 10;
+    bcrypt.hash(newUser.password, saltRounds, function(err, hash) {
+      newUser.password = hash;
+      collection
+      .insertOne(newUser)
+      .then((result) => {
+      console.log(result);
+      res.json(result.ops[0])
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
+});
 
   return router;
 };
