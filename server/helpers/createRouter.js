423fc2002fb0deffbc57d17f9e4edac816f93c54
Debbie Urquhart <debiurquhart@live.co.uk>
2021-04-17 14:33:52 +0100

    validates user email is unique

--- server/helpers/createRouter.js
+++ server/helpers/createRouter.js
@@ -17,46 +17,53 @@ const createRouter = function(collection) {
       });
   });
 
-  router.post("/register", (req, res)=> {
+  router.post("/register", async (req, res) => {
     const newUser = req.body;
     const saltRounds = 10;
-    bcrypt.hash(newUser.password, saltRounds, function(err, hash) {
-      newUser.password = hash;
-      collection
-      .insertOne(newUser)
-      .then((result) => {
-      console.log(result);
-      res.json(result.ops[0])
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500);
-      res.json({ status: 500, error: err });
-    });
-  });
+    const foundUser = await collection.findOne({ email: newUser.email });
+    if (!foundUser) {
+      bcrypt.hash(newUser.password, saltRounds, function(err, hash) {
+        newUser.password = hash;
+        collection
+          .insertOne(newUser)
+          .then(result => {
+            console.log(result);
+            res.json(result.ops[0]);
+            res.status(500);
+          })
+          .catch(err => {
+            console.error(err);
+            res.status(500);
+            res.json({ status: 500, error: err });
+          });
+      });
+    } else {
+      res.status(400);
+      res.json({ status: 400, error: " User already exists" });
+    }
   });
 
   router.post("/login", async (req, res) => {
     const user = req.body;
-    console.log("user:", user);
+    const foundUser = await collection.findOne({ email: user.email });
 
-    const foundUser = await collection.findOne({email: user.email})
-    console.log("found user:", foundUser);
+    if (foundUser) {
+      const validPassword = bcrypt.compare(user.password, foundUser.password);
 
-    if (foundUser){
-      const validPassword = bcrypt.compare(user.password, foundUser.password)
-
-      if(validPassword){
-        res.status(200).json({message:"Valid Password", status: 200})
+      if (validPassword) {
+        res.status(200).json({ message: "Valid Password", status: 200 });
+      } else {
+        res.status(400).json({ error: "Invalid password", status: 400 });
       }
-      else { 
-        res.status(400).json({error: "Invalid password", status: 400})
-      }
-    // if user not found:
+      // if user not found:
     } else {
-      res.status(401).json({error: "User does not exist", status:401})
+      res.status(401).json({ error: "User does not exist", status: 401 });
     }
-  })
+  });
+
+  router.get("/:id", (req, res) => {
+    const id = req.params.id;
+  });
 
   return router;
 };
