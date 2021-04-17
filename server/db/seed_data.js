7ec3d2afae570a24a01d43d4fab00f2fe774b35f
Debbie Urquhart <debiurquhart@live.co.uk>
2021-04-17 09:54:08 +0100

    adds seed data

--- server/db/seed_data.js
+++ server/db/seed_data.js
@@ -1,12 +1,36 @@
-const User = require("../models/UserModel");
 
-const lizzie = new User({
-        "firstName": "Lizzie",
-        "lastName": "Bennett",
-        "email": "lb@email.com",
-        "password": "qwerty",
-        "created": Date.now(),
-        "verified": True
-})
-lizzie.save().then(console.log("lizzie has been saved"));
+// const User = require("../models/UserModel");
 
+// const lizzie = new User({
+//         "firstName": "Lizzie",
+//         "lastName": "Bennett",
+//         "email": "lb@email.com",
+//         "password": "qwerty",
+//         "created": Date.now(),
+//         "verified": True
+// })
+// lizzie.save().then(console.log("lizzie has been saved"));
+
+use clothX;
+
+db.dropDatabase();
+
+db.users.insertMany([
+  {
+    firstName: "Lizzie",
+    lastName: "Bennett",
+    email: "lb@email.com",
+    password: "qwerty",
+    created: Date.now(),
+    verified: true
+  },
+
+  {
+    firstName: "Charles",
+    lastName: "Bingley",
+    email: "cb@email.com",
+    password: "Passw0rd!",
+    created: Date.now(),
+    verified: false
+  }
+]);
---
+++ server/helpers/createRouter.js
@@ -0,0 +1,2 @@
+const express = require("express");
+const ObjectId = require("mongodb").ObjectID;
